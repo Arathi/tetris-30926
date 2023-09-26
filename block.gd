3db2b506086fb4d425c90ca6bb3ddded0698f0a0
Arathi <ssdfa7@gmail.com>
2023-09-26 14:55:41 +0800

    上色
    Signed-off-by: Arathi <ssdfa7@gmail.com>

--- block.gd
+++ block.gd
@@ -30,6 +30,68 @@ const direction_270 = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if get_meta("falling"):
+		var type = get_meta("type")
+		var color: Color = Color("#BCBCBC")
+		var shadowColor: Color = Color("#747474")
+		var lightColor: Color = Color("#FCFCFC")
+		
+		if type == BLOCK_TYPE_I: # v
+			color = Color("#D82800")
+			shadowColor = Color("#A40000")
+			lightColor = Color("#FC7460")
+			pass
+		elif type == BLOCK_TYPE_J: #v
+			color = Color("#FC9838")
+			shadowColor = Color("#C84C0C")
+			lightColor = Color("#FCD8A8")
+			pass
+		elif type == BLOCK_TYPE_L: # v
+			color = Color("#F478FC")
+			shadowColor = Color("#BC00BC")
+			lightColor = Color("#FCC4FC")
+			pass
+		elif type == BLOCK_TYPE_O: # v
+			color = Color("#2038EC")
+			shadowColor = Color("#24188C")
+			lightColor = Color("#3CBCFC")
+			pass
+		elif type == BLOCK_TYPE_S: #v
+			color = Color("#00A800")
+			shadowColor = Color("#005000")
+			lightColor = Color("#4CDC48")
+			pass
+		elif type == BLOCK_TYPE_T: # v
+			color = Color("#887000")
+			shadowColor = Color("#402C00")
+			lightColor = Color("#FC9838")
+			pass
+		elif type == BLOCK_TYPE_Z: # v
+			color = Color("#008088")
+			shadowColor = Color("#183C5C")
+			lightColor = Color("#00E8D8")
+			pass
+		
+		get_node("tile1").set_meta("color", color)
+		get_node("tile2").set_meta("color", color)
+		get_node("tile3").set_meta("color", color)
+		get_node("tile4").set_meta("color", color)
+		
+		get_node("tile1").set_meta("shadowColor", shadowColor)
+		get_node("tile2").set_meta("shadowColor", shadowColor)
+		get_node("tile3").set_meta("shadowColor", shadowColor)
+		get_node("tile4").set_meta("shadowColor", shadowColor)
+		
+		get_node("tile1").set_meta("lightColor", lightColor)
+		get_node("tile2").set_meta("lightColor", lightColor)
+		get_node("tile3").set_meta("lightColor", lightColor)
+		get_node("tile4").set_meta("lightColor", lightColor)
+		
+		print("方块颜色初始化完成：#%s #%s #%s" % [
+			color.to_html().substr(0,6), 
+			shadowColor.to_html().substr(0,6), 
+			lightColor.to_html().substr(0,6)
+		])
 	pass # Replace with function body.
 
 
@@ -57,17 +119,17 @@ func _process(delta):
 			move_block(1, 1, 1, false, true, false, true)
 			move_block(2, 1, 2, false, true, true, false)
 			move_block(3, 0, 2, true,  false, false, false)
-		if direction == direction_90:
+		elif direction == direction_90:
 			move_block(0, 0, 0, true,  false, false, false)
 			move_block(1, 1, 0, true,  false, true,  false)
 			move_block(2, 2, 0, false, false, true,  true)
 			move_block(3, 2, 1, false, true, false, false)
-		if direction == direction_180:
+		elif direction == direction_180:
 			move_block(0, 1, 0, false, false, true, false)
 			move_block(1, 0, 0, true, false, false, true)
 			move_block(2, 0, 1, false, true, false, true)
 			move_block(3, 0, 2, false, true, false, false)
-		if direction == direction_270:
+		elif direction == direction_270:
 			move_block(0, 0, 0, false, false, false, true)
 			move_block(1, 0, 1, true, true, false, false)
 			move_block(2, 1, 1, true, false, true, false)
@@ -77,11 +139,11 @@ func _process(delta):
 		direction = direction % 4
 		if direction == direction_0:
 			pass
-		if direction == direction_90:
+		elif direction == direction_90:
 			pass
-		if direction == direction_180:
+		elif direction == direction_180:
 			pass
-		if direction == direction_270:
+		elif direction == direction_270:
 			pass
 	
 	if type == BLOCK_TYPE_O: # O
@@ -99,33 +161,33 @@ func _process(delta):
 			move_block(2, 1, 1, false, true, true, false)
 			move_block(3, 0, 1, true, false, false, false)
 			pass
-		if direction == direction_90:
+		elif direction == direction_90:
 			pass
-		if direction == direction_180:
+		elif direction == direction_180:
 			pass
-		if direction == direction_270:
+		elif direction == direction_270:
 			pass
 	
 	if type == block_type_t: # T
 		direction = direction % 4
 		if direction == direction_0:
 			pass
-		if direction == direction_90:
+		elif direction == direction_90:
 			pass
-		if direction == direction_180:
+		elif direction == direction_180:
 			pass
-		if direction == direction_270:
+		elif direction == direction_270:
 			pass
 	
 	if type == block_type_z: # Z
 		direction = direction % 4
 		if direction == direction_0:
 			pass
-		if direction == direction_90:
+		elif direction == direction_90:
 			pass
-		if direction == direction_180:
+		elif direction == direction_180:
 			pass
-		if direction == direction_270:
+		elif direction == direction_270:
 			pass
 
 
--- block_tile.gd
+++ block_tile.gd
@@ -4,11 +4,39 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	self.set_colors()
+	self.show_or_hide()
+
+
+func set_colors():
+	var color = get_meta("color")
+	var shadowColor = get_meta("shadowColor")
+	var lightColor = get_meta("lightColor")
+	
+	get_node("core/block").color = color
+	get_node("right/block").color = color
+	get_node("top/block").color = color
+	get_node("left/block").color = color
+	get_node("bottom/block").color = color
+	
+	get_node("core/shadow").color = shadowColor
+	get_node("right/shadow").color = shadowColor
+	get_node("bottom/shadow").color = shadowColor
+	
+	get_node("right/light").color = lightColor
+	get_node("top/light").color = lightColor
+	get_node("left/light").color = lightColor
+	get_node("bottom/light").color = lightColor
+	get_node("not_right/light").color = lightColor
+	get_node("not_top/light").color = lightColor
+
+
+func show_or_hide():
 	var rightNode = self.get_child(1)
 	var notRightNode = self.get_child(5)
 	if get_meta("right"):
